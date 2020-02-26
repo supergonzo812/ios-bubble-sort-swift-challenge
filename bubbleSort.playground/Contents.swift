2107509079837ca0541943eb468fce0a4bbd390e
Chris <chris.gonzales.82@gmail.com>
2020-02-26 08:39:21 -0800

    Challenge MVP complete

--- bubbleSort.playground/Contents.swift
+++ bubbleSort.playground/Contents.swift
@@ -1,16 +1,39 @@
 import UIKit
 
-func bubbleSort(){
+
+
+func bubbleSort(_ array:inout [Int], by: Character = "<"){
     
+    if by == ">" {
+    for first in 0..<array.count{
+        for index in 1..<array.count - first{
+            if array[index] > array[(index - 1)] {
+                let temp = array[(index)]
+                array[(index)] = array[index - 1]
+                array[index - 1] = temp
+            }
+        }
+    }
+    } else {
+        for first in 0..<array.count{
+            for index in 1..<array.count - first{
+                if array[index] < array[(index - 1)] {
+                    let temp = array[(index - 1)]
+                    array[(index - 1)] = array[index]
+                    array[index] = temp
+                }
+            }
+        }
+    }
 }
 
 
 // Test Cases
-//var arr1 = [1, 3, 6, 2, 4, 5]
-//var arr3 = [6, 5, 4, 3, 2, 1]
-//bubbleSort(&arr1) // returns [1, 2, 3, 4, 5, 6]
-//bubbleSort(&arr1, by: >) // returns [6, 5, 4, 3, 2, 1]
-//bubbleSort(&arr3) // returns [6, 5, 4, 3, 2, 1]
+var arr1 = [1, 3, 6, 2, 4, 5]
+var arr3 = [6, 5, 4, 3, 2, 1]
+bubbleSort(&arr1) // returns [1, 2, 3, 4, 5, 6]
+bubbleSort(&arr1, by: ">") // returns [6, 5, 4, 3, 2, 1]
+bubbleSort(&arr3) // returns [6, 5, 4, 3, 2, 1]
 
 
 // Test Cases (Stretch Goal)
